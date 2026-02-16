26daa1eec9404eb8d5a457add890bb1c8b462e6b
benvarzam <benje1612@gmail.com>
2026-02-16 17:03:06 +0100

    initial commit

---
+++ src/main/java/org/nttdata/spring/SpringReservasNttDataApplication.java
@@ -0,0 +1,13 @@
+package org.nttdata.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReservasNttDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReservasNttDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nttdata/spring/SpringReservasNttDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nttdata.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReservasNttDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
