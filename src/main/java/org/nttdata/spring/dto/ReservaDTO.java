d94479ea523499d928a7322f9cccf5be2ba3ae25
davberamo <dberlinches2003@gmail.com>
2026-02-23 16:41:43 +0100

    Parseando fecha

--- src/main/java/org/nttdata/spring/dto/ReservaDTO.java
+++ src/main/java/org/nttdata/spring/dto/ReservaDTO.java
@@ -1,5 +1,6 @@
 package org.nttdata.spring.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -15,9 +16,11 @@ public class ReservaDTO {
     @NotNull(message = "El ID de puesto es obligatorio")
     private Integer idPuesto;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @NotNull(message = "La fecha de inicio es obligatoria")
     private LocalDateTime fechaInicio;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @NotNull(message = "La fecha final es obligatoria")
     private LocalDateTime fechaFinal;
 
