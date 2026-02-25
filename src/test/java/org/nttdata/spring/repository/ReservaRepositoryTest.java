7c9bc0ac0506e977c964593528e46a50cecb133b
Benji161220 <benje1612@gmail.com>
2026-02-25 23:42:15 +0100

    Refactor entidades y servicios para usar relaciones JPA en lugar de IDs directos

--- src/test/java/org/nttdata/spring/repository/ReservaRepositoryTest.java
+++ src/test/java/org/nttdata/spring/repository/ReservaRepositoryTest.java
@@ -25,15 +25,16 @@ class ReservaRepositoryTest {
     private ReservaRepository reservaRepository;
 
     /*
-    @Contaniner
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
+     * @Contaniner
+     * static PostgreSQLContainer<?> postgres = new
+     * PostgreSQLContainer<>("postgres:15-alpine")
+     * 
+     * @DynamicPropertySource
+     * static void configureProperties(DynamicPropertyRegistry registry) {
+     * registry.add("spring.datasource.url", postgres::getJdbcUrl);
+     * registry.add("spring.datasource.username", postgres::getUsername);
+     * registry.add("spring.datasource.password", postgres::getPassword);
+     * }
      */
 
     @Test
